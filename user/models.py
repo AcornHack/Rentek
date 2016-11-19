0d65ba53e3020e5663568f0759b1f6070e0e8a0c
John <kongj016.202@uclacademy.org>
2016-11-19 21:49:42 +0000

    v4

--- user/models.py
+++ user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CustomUser(User):
+	address = CharField()
+	credit_number = CharField()
+	security_code = CharField()
+	expiry_date = CharField()
+	card_type = CharField()
+	phone_number = CharField()
+
+	@classmethod
+	def create(cls,address,credit_number,security_code,expiry_date,card_type,phone_number):
+		customuser = cls(address=address,credit_number=credit_number,security_code=security_code,expiry_date=expiry_date,card_type=card_type,phone_number=phone_number)
+		customuser.save()
+		return(customuser)
